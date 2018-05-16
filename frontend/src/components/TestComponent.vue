c6d885df96746603e35ade8dc89034b20b74c09e
Norwin Roosen <github@nroo.de>
2018-05-16 16:30:03 +0200

    add frontend template via vue-cli

---
+++ frontend/src/components/TestComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+        <blockquote>
+          &#8220;HEWWO WORWD!&#8221;
+          <footer>
+            <small>
+              <em>&mdash;me</em>
+            </small>
+          </footer>
+        </blockquote>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
