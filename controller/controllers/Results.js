1a5fb6f93fe15aa67768b94d1e388b2a2cd177e4
Norwin Roosen <github@nroo.de>
2018-05-22 09:41:31 +0200

    controller: update API spec

--- controller/controllers/Results.js
+++ controller/controllers/Results.js
@@ -15,10 +15,26 @@ module.exports.deleteResults = function deleteResults (req, res, next) {
     });
 };
 
+module.exports.getResultCount = function getResultCount (req, res, next) {
+  var crawls = req.swagger.params['crawls'].value;
+  var query = req.swagger.params['query'].value;
+  Results.getResultCount(crawls,query)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
 module.exports.getResults = function getResults (req, res, next) {
   var crawls = req.swagger.params['crawls'].value;
   var query = req.swagger.params['query'].value;
-  Results.getResults(crawls,query)
+  var maxResults = req.swagger.params['maxResults'].value;
+  var page = req.swagger.params['page'].value;
+  var format = req.swagger.params['format'].value;
+  var download = req.swagger.params['download'].value;
+  Results.getResults(crawls,query,maxResults,page,format,download)
     .then(function (response) {
       utils.writeJson(res, response);
     })
--- controller/service/ResultsService.js
+++ controller/service/ResultsService.js
@@ -7,7 +7,7 @@
  *
  * crawls String Crawl IDs to filter results (optional)
  * query String Elastic Search query term to filter results (optional)
- * returns inline_response_200
+ * returns DeleteResponse
  **/
 exports.deleteResults = function(crawls,query) {
   return new Promise(function(resolve, reject) {
@@ -25,14 +25,39 @@ exports.deleteResults = function(crawls,query) {
 
 
 /**
+ * Get count of crawl results with optional filtering
+ * 
+ *
+ * crawls String Crawl IDs to filter results (optional)
+ * query String Elastic Search query term to filter results (optional)
+ * returns Integer
+ **/
+exports.getResultCount = function(crawls,query) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = 0;
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
  * Get crawl results with optional filtering
  * 
  *
  * crawls String Crawl IDs to filter results (optional)
  * query String Elastic Search query term to filter results (optional)
+ * maxResults Integer The amount of results to return (optional)
+ * page Integer To page through the results (optional)
+ * format String Format in which results are returned (optional)
+ * download Boolean Whether to send an attachment header (optional)
  * returns List
  **/
-exports.getResults = function(crawls,query) {
+exports.getResults = function(crawls,query,maxResults,page,format,download) {
   return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = [ {
@@ -40,20 +65,20 @@ exports.getResults = function(crawls,query) {
     "contact" : "contact"
   },
   "scores" : {
-    "dataportal" : 6.02745618307040320615897144307382404804229736328125,
-    "dataset" : 0.80082819046101150206595775671303272247314453125
+    "dataportal" : 6.027456183070403,
+    "dataset" : 0.8008281904610115
   },
-  "url" : "046b6c7f-0b8a-43b9-b35d-6489e6daee91",
+  "url" : "https://example.com/foobar",
   "crawl" : "046b6c7f-0b8a-43b9-b35d-6489e6daee91"
 }, {
   "metadata" : {
     "contact" : "contact"
   },
   "scores" : {
-    "dataportal" : 6.02745618307040320615897144307382404804229736328125,
-    "dataset" : 0.80082819046101150206595775671303272247314453125
+    "dataportal" : 6.027456183070403,
+    "dataset" : 0.8008281904610115
   },
-  "url" : "046b6c7f-0b8a-43b9-b35d-6489e6daee91",
+  "url" : "https://example.com/foobar",
   "crawl" : "046b6c7f-0b8a-43b9-b35d-6489e6daee91"
 } ];
     if (Object.keys(examples).length > 0) {
