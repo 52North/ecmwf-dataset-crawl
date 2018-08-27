52North/ecmwf-dataset-crawl

commit 68346908591e6764984cf636ddebfddc3918c621

add n52 namespace to internal metadata

crawler/crawler-conf.flux
@@ -36,6 +36,7 @@ config:
 
   # lists the metadata to persist to storage
   # these are not transfered to the outlinks
+  # only values with unchanged names are listed here, remaining in indexer.md.mapping
   metadata.persist:
    - crawl
    - "parse.title"
@@ -44,6 +45,7 @@ config:
    - error.source
    - isSitemap
    - isFeed
+   - host
 
   http.agent.name: "ECMWF Dataset Crawler"
   http.agent.version: "0.1-alpha"
@@ -90,29 +92,30 @@ config:
   indexer.url.fieldname: "url"
   indexer.text.fieldname: "content"
   indexer.canonical.name: "canonical"
-  indexer.md.mapping:
-  - crawl=crawl # from init
 
+  # map internal metadata names to indexed attribute names
+  # syntax: internal=indexed
+  indexer.md.mapping:
   # storm crawler default filters
   - parse.title=title
   - parse.description=description
-  - host=host
 
   - language=language # from LanguageDetectionFilter
 
-  - classify.class=classification.auto
-  - classify.confidence=classification.confidence
+  - n52.language=language
+  - n52.classify.class=classification.auto
+  - n52.classify.confidence=classification.confidence
 
   # directly from parsefilters.json (XPath3Filter)
-  - extracted.data_portal=extracted.data_portal
-  - extracted.data_api=extracted.data_api
-  - extracted.data_link=extracted.data_link
-  - extracted.data_pdf=extracted.data_pdf
-  - extracted.license=extracted.license
-  - extracted.contact=extracted.contact
+  - n52.extracted.data_portal=extracted.data_portal
+  - n52.extracted.data_api=extracted.data_api
+  - n52.extracted.data_link=extracted.data_link
+  - n52.extracted.data_pdf=extracted.data_pdf
+  - n52.extracted.license=extracted.license
+  - n52.extracted.contact=extracted.contact
 
-  - keywords=keywords # merged keyword list from ScoringBolt
-  - score=score # from ScoringBolt
+  - n52.keywords=keywords # merged keyword list from ScoringBolt
+  - n52.score=score # from ScoringBolt
 
   status.updater.cache.spec: "maximumSize=500000,expireAfterAccess=1h"
 

crawler/src/main/java/org/n52/webcrawl/CrawlStatusUpdaterBolt.java
@@ -354,7 +354,7 @@ public class CrawlStatusUpdaterBolt extends AbstractStatusUpdaterBolt implements
     }
 
     protected String getIndexName(Metadata md) {
-        String crawlId = md.getFirstValue("crawl");
+        String crawlId = md.getFirstValue("n52.crawl.id");
         return indexName.replace("*", crawlId.toLowerCase());
     }
 }

crawler/src/main/java/org/n52/webcrawl/LanguageDetectionFilter.java
@@ -40,7 +40,7 @@ public class LanguageDetectionFilter extends ParseFilter {
             if (text == null) return;
             LanguageIdentifier result = new LanguageIdentifier(text);
 //            if (result.isReasonablyCertain()) {
-                metadata.addValue("language", result.getLanguage());
+                metadata.addValue("n52.language", result.getLanguage());
 //            }
 
             // new tika method, considering the whole document? "fails with no language detectors available"

crawler/src/main/java/org/n52/webcrawl/LuceneScoreFilter.java
@@ -22,9 +22,9 @@ import java.util.*;
  * Topic definitions are retrieved from a JSON file
  * TODO: in SC 1.10 we can use JSONResourceWrapper to get them from an Elasticsearch index!
  */
-public class SimpleTopicScoreParseFilter extends ParseFilter {
+public class LuceneScoreFilter extends ParseFilter {
     private static final org.slf4j.Logger LOG = LoggerFactory
-            .getLogger(SimpleTopicScoreParseFilter.class);
+            .getLogger(LuceneScoreFilter.class);
 
     private MemoryIndex index;
 

crawler/src/main/java/org/n52/webcrawl/ScoringBolt.java
@@ -32,10 +32,10 @@ public class ScoringBolt extends BaseBasicBolt {
         Metadata m = (Metadata) input.getValueByField("metadata");
 
         float score = calcScore(m);
-        m.setValue("score", Float.toString(score));
+        m.setValue("n52.score", Float.toString(score));
 
         List<String> keywords = generateKeywords(m);
-        m.setValues("keywords", keywords.toArray(new String[keywords.size()]));
+        m.setValues("n52.keywords", keywords.toArray(new String[keywords.size()]));
 
         collector.emit(new Values(input.getValueByField("url"), m, input.getValueByField("text")));
     }
@@ -49,17 +49,17 @@ public class ScoringBolt extends BaseBasicBolt {
     private List<String> generateKeywords (Metadata m) {
         List<String> keywords = new ArrayList<>();
 
-        if (m.getFirstValue("extracted.data_portal") != null) keywords.add("dataportal");
-        if (m.getFirstValue("extracted.data_api") != null)    keywords.add("api");
-        if (m.getFirstValue("extracted.data_link") != null)   keywords.add("datalink");
-        if (m.getFirstValue("extracted.data_pdf") != null)   keywords.add("pdf");
+        if (m.getFirstValue("n52.extracted.data_portal") != null) keywords.add("dataportal");
+        if (m.getFirstValue("n52.extracted.data_api") != null)    keywords.add("api");
+        if (m.getFirstValue("n52.extracted.data_link") != null)   keywords.add("datalink");
+        if (m.getFirstValue("n52.extracted.data_pdf") != null)   keywords.add("pdf");
 
         String keyword;
-        keyword = m.getFirstValue("keywords.dataset");
+        keyword = m.getFirstValue("n52.keywords.dataset");
         if (keyword != null && !keyword.equals("0.0")) keywords.add("dataset");
-        keyword = m.getFirstValue("keywords.historic");
+        keyword = m.getFirstValue("n52.keywords.historic");
         if (keyword != null && !keyword.equals("0.0")) keywords.add("historic");
-        keyword = m.getFirstValue("keywords.realtime");
+        keyword = m.getFirstValue("n52.keywords.realtime");
         if (keyword != null && !keyword.equals("0.0")) keywords.add("realtime");
 
         String[] keyws = m.getValues("parse.keywords");
@@ -74,30 +74,30 @@ public class ScoringBolt extends BaseBasicBolt {
         Float score = 0.0f;
 
         // class from DatasetClassificationBolt (python)
-        String clazz = m.getFirstValue("classify.class");
-        String confidence = m.getFirstValue("classify.confidence");
+        String clazz = m.getFirstValue("n52.classify.class");
+        String confidence = m.getFirstValue("n52.classify.confidence");
         if ("dataset".equals(clazz)) score += weightClassify;
         if (clazz == null) score += 0.5f * weightClassify; // boost unclassified results so they don't get lost
         if (confidence != null) score += Float.parseFloat(confidence); // not normalized! negative scoring for "unrelated"
 
-        // from SimpleTopicScoreParseFilter
+        // from LuceneScoreFilter
         String keyword;
-        keyword = m.getFirstValue("keywords.historic");
+        keyword = m.getFirstValue("n52.keywords.historic");
         if (keyword != null) score += weightHistoric * Float.parseFloat(keyword);
-        keyword = m.getFirstValue("keywords.realtime");
+        keyword = m.getFirstValue("n52.keywords.realtime");
         if (keyword != null) score += weightRealtime * Float.parseFloat(keyword);
-        keyword = m.getFirstValue("keywords.dataset");
+        keyword = m.getFirstValue("n52.keywords.dataset");
         if (keyword != null) score += weightDataset * Float.parseFloat(keyword);
 
         // from XPath3Filter
         String[] extractlist;
-        extractlist = m.getValues("extracted.data_api");
+        extractlist = m.getValues("n52.extracted.data_api");
         if (extractlist != null) score += weightExtract * Math.max(extractlist.length, 2);
-        extractlist = m.getValues("extracted.data_link");
+        extractlist = m.getValues("n52.extracted.data_link");
         if (extractlist != null) score += weightExtract * Math.max(extractlist.length, 2);
-        extractlist = m.getValues("extracted.data_portal");
+        extractlist = m.getValues("n52.extracted.data_portal");
         if (extractlist != null) score += weightExtract * Math.max(extractlist.length, 2);
-        extractlist = m.getValues("extracted.license");
+        extractlist = m.getValues("n52.extracted.license");
         if (extractlist != null) score += weightExtract * Math.max(extractlist.length, 2);
 
         return score;

crawler/src/main/resources/resources/dataset_classifier_bolt.py
@@ -50,7 +50,7 @@ class DatasetClassifierBolt(storm.BasicBolt):
         url, metadata, text = tup.values
         metadata = Metadata(metadata)
         try:
-            lang = metadata['language'][0]
+            lang = metadata['n52.language'][0]
         except KeyError:
             lang = 'unknown'
 
@@ -65,8 +65,8 @@ class DatasetClassifierBolt(storm.BasicBolt):
         logging.debug(metadata)
 
         confidence, clazz = self.classify([text])[0]
-        metadata["classify.class"] = clazz
-        metadata["classify.confidence"] = confidence
+        metadata["n52.classify.class"] = clazz
+        metadata["n52.classify.confidence"] = confidence
 
         logging.debug([text[100:200], confidence, clazz])
         storm.emit([url, metadata, text], anchors=[tup])

crawler/src/test/java/org/n52/webcrawl/CrawlStatusUpdaterBoltTest.java
@@ -15,7 +15,7 @@ public class CrawlStatusUpdaterBoltTest {
         CrawlStatusUpdaterBolt bolt = new CrawlStatusUpdaterBolt(statusIndex);
 
         Metadata md = new Metadata();
-        md.setValue("crawl", "tEstCrawl");
+        md.setValue("n52.crawl.id", "tEstCrawl");
 
         assertEquals(bolt.getIndexName(md), "crawlstatus-testcrawl");
         assertEquals(bolt.getIndexName(md), "crawlstatus-testcrawl");
@@ -27,7 +27,7 @@ public class CrawlStatusUpdaterBoltTest {
         CrawlStatusUpdaterBolt bolt = new CrawlStatusUpdaterBolt(statusIndex);
         Metadata md = new Metadata();
 
-        md.setValue("crawl", "tEstCrawl");
+        md.setValue("n52.crawl.id", "tEstCrawl");
         assertEquals(bolt.getIndexName(md), "crawlstatus");
     }
 

crawler/src/test/java/org/n52/webcrawl/LuceneScoreFilterTest.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class SimpleTopicScoreParseFilterTest extends ParsingTester {
+public class LuceneScoreFilterTest extends ParsingTester {
 
     @Before
     public void setupParserBolt() {
@@ -43,7 +43,7 @@ public class SimpleTopicScoreParseFilterTest extends ParsingTester {
 
     @Test
     public void testMatch() throws ParseException {
-        SimpleTopicScoreParseFilter filter = new SimpleTopicScoreParseFilter();
+        LuceneScoreFilter filter = new LuceneScoreFilter();
         filter.createIndex("This text is all about testing.");
         Assert.assertTrue("match returns > 0", filter.search("te?t") > 0);
         Assert.assertTrue("no match returns 0", filter.search("no match") == 0);
@@ -51,7 +51,7 @@ public class SimpleTopicScoreParseFilterTest extends ParsingTester {
 
     @Test(expected = ParseException.class)
     public void testQueryException() throws ParseException {
-        SimpleTopicScoreParseFilter filter = new SimpleTopicScoreParseFilter();
+        LuceneScoreFilter filter = new LuceneScoreFilter();
         filter.createIndex("asdf");
         filter.search("wat90)(&*r23j4kewelhfla38w??#@?!$3");
     }
@@ -98,7 +98,7 @@ public class SimpleTopicScoreParseFilterTest extends ParsingTester {
     }
 
     float printQuery(String text, String query) throws ParseException {
-        SimpleTopicScoreParseFilter filter = new SimpleTopicScoreParseFilter();
+        LuceneScoreFilter filter = new LuceneScoreFilter();
         filter.createIndex(text);
         float score = filter.search(query);
         System.out.println("\ntext:\t" + text + "\nquery:\t" + query + "\nscore:\t" + score);

commit 955c4b885db1e93ffc3bae867df33f787bb4d033

fixes for kibana
regression from 68346908591e6764984cf636ddebfddc3918c621
and 90d1deb06cd54298d6e0b7990508656ddeacb6d6

crawler/crawler-conf.flux
@@ -39,13 +39,11 @@ config:
   # these are not transfered to the outlinks
   # only values with unchanged names are listed here, remaining in indexer.md.mapping
   metadata.persist:
-   - "parse.title"
    - _redirTo
    - error.cause
    - error.source
    - isSitemap
    - isFeed
-   - host
 
   http.agent.name: "ECMWF Dataset Crawler"
   http.agent.version: "0.1-alpha"
@@ -99,6 +97,7 @@ config:
   # storm crawler default filters
   - parse.title=title
   - parse.description=description
+  - host=host
 
   - n52.crawl.id=crawl.id
   - n52.crawl.languages=crawl.languages